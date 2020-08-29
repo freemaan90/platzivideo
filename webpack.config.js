89cdcdf00f4992dba0915c571e6fc1e9186a2638
Frederico Zanoni <fredericozanoni@gmail.com>
2020-08-29 19:22:51 -0300

    Agrege eslint

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 module.exports = {
 	entry: './src/index.js',
 	output: {
@@ -23,6 +24,16 @@ module.exports = {
 				use: {
 					loader: 'html-loader'
 				}
+			},
+			{
+				test: /\.(s*)css$/,
+				use: [
+					{
+						loader: MiniCssExtractPlugin.loader
+					},
+					'css-loader',
+					'sass-loader'
+				]
 			}
 		]
 	},
@@ -30,6 +41,9 @@ module.exports = {
 		new HtmlWebpackPlugin({
 			template: './public/index.html',
 			filename: './index.html'
+		}),
+		new MiniCssExtractPlugin({
+			filename: 'assets/[name].css'
 		})
 	]
 };
